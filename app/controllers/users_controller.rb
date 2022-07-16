56de0852d7aa48ac695d7fba52bff13b977a8fb1
unknown <didiersangwa@gmail.com>
2022-07-16 13:40:07 +0200

    Fix some tests issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:user_id])
-  end
-
   def index
     @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @users }
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @recent_posts = @user.recent_3_posts
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
+    before(:example) { get '/users' }
+    it 'responds with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders users list view' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'shows correct content in the view' do
+      expect(response.body).to include('List of users')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
+    before(:example) { get '/users/1' }
+    it 'responds with code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'renders users list view' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'shows correct content in the view' do
+      expect(response.body).to include('User Details')
     end
   end
 end
