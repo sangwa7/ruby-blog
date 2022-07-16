44b52d3922ba72a8f3445c0e96ce3b6df859ae9e
unknown <didiersangwa@gmail.com>
2022-07-16 21:12:52 +0200

    Fix required changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show] do
-      resources :comments, only: [:create :new]
+      resources :comments, only: [:create, :new]
       resources :likes, only: [:create, :destroy]
   end
 end
