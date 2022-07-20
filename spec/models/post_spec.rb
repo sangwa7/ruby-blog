3f4b8504f7bd590294866644df4321403bdb3b74
unknown <didiersangwa@gmail.com>
2022-07-20 11:01:03 +0200

    Fix offenses

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -15,33 +15,40 @@ RSpec.describe Post, type: :model do
   let(:comment4) { Comment.new(user: the_author, post: the_post, text: 'c4') }
   let(:comment5) { Comment.new(user: the_author, post: the_post, text: 'c5') }
   let(:comment6) { Comment.new(user: the_author, post: the_post, text: 'c6') }
+
   context 'post.title' do
     it 'has some value' do
       the_post.title = nil
       expect(the_post).to_not be_valid
     end
+
     it 'is not blank' do
       the_post.title = '    '
       expect(the_post).to_not be_valid
     end
+
     it 'cannot be longer than 250 characters' do
       the_post.title = 'a' * 251
       expect(the_post).to_not be_valid
     end
+
     it 'accepts "The post title"' do
       the_post.title = 'The post title'
       expect(the_post).to be_valid
     end
   end
+
   context 'post.comments_counter' do
     it 'has some value' do
       the_post.comments_counter = nil
       expect(the_post).to_not be_valid
     end
+
     it 'is integer' do
       the_post.comments_counter = 1.2
       expect(the_post).to_not be_valid
     end
+
     it 'accpets 0 and 10' do
       [0, 10].each do |counter|
         the_post.comments_counter = counter
@@ -49,15 +56,18 @@ RSpec.describe Post, type: :model do
       end
     end
   end
+
   context 'post.likes_counter' do
     it 'has some value' do
       the_post.likes_counter = nil
       expect(the_post).to_not be_valid
     end
+
     it 'is integer' do
       the_post.likes_counter = 1.2
       expect(the_post).to_not be_valid
     end
+
     it 'accpets 0 and 10' do
       [0, 10].each do |counter|
         the_post.likes_counter = counter
@@ -65,10 +75,12 @@ RSpec.describe Post, type: :model do
       end
     end
   end
+
   context 'post.recent_5_comments' do
     it 'returns empty list if there is no comments' do
       expect(the_post.recent_5_comments).to eq []
     end
+
     it 'returns recent five comments in right order' do
       comment2.save!
       comment2.save!
@@ -76,6 +88,7 @@ RSpec.describe Post, type: :model do
       comment4.save!
       comment5.save!
       comment6.save!
+
       the_post.reload
       actual_comments = the_post.recent_5_comments.pluck(:text)
       expected_comments = [
